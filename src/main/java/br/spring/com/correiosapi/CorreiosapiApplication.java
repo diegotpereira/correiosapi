d7e561981054e12efd7e63ae25ea04c86a641e71
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-02-08 14:26:35 -0300

    first commit

---
+++ src/main/java/br/spring/com/correiosapi/CorreiosapiApplication.java
@@ -0,0 +1,13 @@
+package br.spring.com.correiosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreiosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreiosapiApplication.class, args);
+	}
+
+}
